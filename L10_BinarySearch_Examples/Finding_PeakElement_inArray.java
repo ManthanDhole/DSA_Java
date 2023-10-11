938152216e9d80f7a9437185812902b80b14125e
mdhole <manthan.dhole@infor.com>
2023-10-12 00:36:57 +0530

    test

--- L10_BinarySearch_Examples/Finding_PeakElement_inArray.java
+++ L10_BinarySearch_Examples/Finding_PeakElement_inArray.java
@@ -2,8 +2,8 @@
 public class Finding_PeakElement_inArray {
     public static void main(String[] args) {
 //        int[] arr = {1,2,3,4,5,6,7,5,4,3,2,1}; //expected ans: 6
-        int[] arr = {10,34,56,67,78,89,93,100,87,65,52,43,30,0}; //expected ans: 7
-//        int[] arr = {3,7,8,10,3,1};    //expected ans: 3
+//        int[] arr = {10,34,56,67,78,89,93,100,87,65,52,43,30,0}; //expected ans: 7
+        int[] arr = {3,7,8,10,3,1};    //expected ans: 3
 //        int[] arr = {0,1,0};    //expected ans:1
 //        int[] arr = {0,0,0,0,0,0};    //expected ans:-1
 //        int[] arr = {1,2,3,2,1,0,-1};    //expected ans: 2
