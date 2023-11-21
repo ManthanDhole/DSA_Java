e9df3b7f6c32e2a6f2319cf63e7fa2756d34bcf7
mdhole <manthan.dhole@infor.com>
2023-11-21 23:59:29 +0530

    find all missing elements in an Array of some range

---
+++ L12_Sorting_Examples/Find_All_missing_numbers_inArray.java
@@ -0,0 +1,60 @@
+//https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Find_All_missing_numbers_inArray {
+    public static void main(String[] args) {
+//        int[] arr = {9,6,4,2,3,7,10,1};
+        int[] arr = {4,3,2,7,8,2,3,1};
+//        int[] arr = {4,0,2,1};
+//        int[] arr = {0,1};
+        System.out.println("Initial Array Elements: " + Arrays.toString(arr));
+        CycleSortAlgorithm(arr);
+        System.out.println("Final Array Elements: " + Arrays.toString(arr));
+
+        List<Integer> missingNumber = FindAllMissingNumber(arr);
+        System.out.println("The missing number is: " + missingNumber);
+    }
+
+    public static List<Integer> FindAllMissingNumber(int[] arr)
+    {
+        List<Integer> missingNumbers = new ArrayList<Integer>();
+        for(int i = 0; i < arr.length; i++)
+        {
+            int expectedNumber = i+1;
+            if(arr[i] != expectedNumber)
+            {
+                missingNumbers.add(expectedNumber);
+            }
+        }
+        return missingNumbers;
+    }
+    public static void CycleSortAlgorithm(int[] arr)
+    {
+        int i = 0;
+        while(i < arr.length)
+        {
+            int correctIndex = arr[i]-1;
+            int currentIndex = i;
+            //correctIndex must be less than the length of the array or else it will give index out of bound exception
+            //additional check is required for duplicate numbers in the array before swapping
+            if( correctIndex < arr.length && currentIndex != correctIndex && arr[correctIndex] != arr[currentIndex])
+            {
+                swap(arr, currentIndex, correctIndex);
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+    public static void swap(int[] arr, int first, int second)
+    {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+}
