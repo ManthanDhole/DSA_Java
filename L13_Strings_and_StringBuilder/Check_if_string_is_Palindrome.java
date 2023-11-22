034c302782d3b935b5e68e70c5977792ecb53faa
mdhole <manthan.dhole@infor.com>
2023-11-23 00:05:44 +0530

    Added check for null and empty string in palindrome example

--- L13_Strings_and_StringBuilder/Check_if_string_is_Palindrome.java
+++ L13_Strings_and_StringBuilder/Check_if_string_is_Palindrome.java
@@ -2,22 +2,27 @@ public class Check_if_string_is_Palindrome {
     public static void main(String[] args) {
         //Palindrome: a string which when read from start or from end contains the same sequence of character
         //Example:  abcba, leel, teet, baab, etc
-//        String str = "abccba";
-        String str = "abcdecba";
+        String str = "abccba";
+//        String str = null;
+//        String str = "";
+//        String str = "abcdecba";
         boolean isPalindrome = false;
-        for (int i = 0, j = str.length()-1; i<str.length() && j >= 0; i++,j-- )
+
+        if(str == null || str.length() == 0)
         {
-            if(str.charAt(i) != str.charAt(j))
-            {
-                isPalindrome = false;
-                break;
-            }
-            else
-            {
-                isPalindrome = true;
+            isPalindrome = true;
+        }
+        else
+        {
+            for (int i = 0, j = str.length() - 1; i < str.length() && j >= 0; i++, j--) {
+                if (str.charAt(i) != str.charAt(j)) {
+                    isPalindrome = false;
+                    break;
+                } else {
+                    isPalindrome = true;
+                }
             }
         }
-
         System.out.println("String " + str + " is palindrome: " + isPalindrome);
     }
 }
